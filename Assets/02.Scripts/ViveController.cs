393fda8268bbc2d96c2b9e451fe4d758bfa4bf96
zigoom <rlawhdgns72@naver.com>
2020-06-25 15:44:31 +0900

    중복된거 삭제함

---
+++ Assets/02.Scripts/ViveController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveController : MonoBehaviour
+{
+
+    //컨트롤러 정의
+    public SteamVR_Input_Sources leftHand;
+    public SteamVR_Input_Sources rightHand;
+    public SteamVR_Input_Sources any;
+
+    //컨트롤러 입력값 정의
+    public SteamVR_Action_Boolean trigger;
+
+    public SteamVR_Action_Boolean trackPadClick = SteamVR_Actions.default_Teleport;
+    public SteamVR_Action_Boolean trackPadTouch = SteamVR_Actions.default_TrackPadTouch;
+    public SteamVR_Action_Vector2 trackPadPosition = SteamVR_Actions.default_TrackPadPosition;
+
+    void Awake(){
+        trigger = SteamVR_Actions.default_InteractUI;
+    }
+
+    void Update()
+    {
+
+        //왼손 컨트롤러의 트리거 버튼을 클릭했을 때 발생
+        if(trigger.GetStateDown(leftHand)){
+            Debug.Log("Clicked Trigger Button");
+        }
+
+        //오른손 컨트롤러의 트리거 버튼을 클릭했을 때 발생
+        if(trigger.GetStateDown(rightHand)){
+            Debug.Log("Released Trigger Button");
+        }
+
+        //트랙패드 클릭
+        if(trackPadClick.GetStateDown(any)){
+            Debug.Log("TrackPad Clicked");
+        }
+        
+        if(trackPadTouch.GetState(any)){
+            Vector2 pos = trackPadPosition.GetAxis(any);
+            Debug.Log($"Touch Pos x={pos.x}/y={pos.y}");    //c# 6.0
+            //Debug.LogFormat($"Touch Pos x={0}/y={1}",pos.x,pos.y);    //c# 6.0
+        }
+
+        
+    }
+}
